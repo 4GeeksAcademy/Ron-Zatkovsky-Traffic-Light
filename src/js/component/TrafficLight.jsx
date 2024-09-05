2188f0d5f4f893f078d2bf93c002a6d600c8cd5a
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-09-05 01:19:55 +0000

    added bonus stuff

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,14 +1,12 @@
 import React from "react"
-import { useState } from "react"
 
-export default function TrafficLight(){
-    const [color,setColor]=useState("red")
-
+export default function TrafficLight(props){
     return(
         <div className="row flex-column mx-auto px-5">
-            <button onClick={()=>setColor("red")} id="red" className={`col my-2 bg-danger p-5 border rounded-circle ${color=="red" ? "glow":""}`} ><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
-            <button onClick={()=>setColor("yellow")} id="yellow" className={`col my-2 bg-warning p-5 border rounded-circle ${color=="yellow" ? "glow":""}`}><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
-            <button onClick={()=>setColor("green")} id="green" className={`col my-2 bg-success p-5 border rounded-circle ${color=="green" ? "glow":""}`}><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
+            <button onClick={()=>props.setColor("red")} id="red" className={`col my-2 bg-danger p-5 border rounded-circle ${props.color=="red" ? "glow":""}`} ><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
+            <button onClick={()=>props.setColor("yellow")} id="yellow" className={`col my-2 bg-warning p-5 border rounded-circle ${props.color=="yellow" ? "glow":""}`}><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
+            <button onClick={()=>props.setColor("green")} id="green" className={`col my-2 bg-success p-5 border rounded-circle ${props.color=="green" ? "glow":""}`}><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>
+            {props.purple? <button onClick={()=>props.setColor("purple")} id="purple" style={{backgroundColor:"purple"}} className={`col my-2 p-5 border rounded-circle ${props.color=="purple" ? "glow":""}`}><br></br><br></br><br></br><br></br><br></br><br></br><br></br></button>:null}
         </div>
     )
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react"
 import TrafficLight from "./TrafficLight";
 
 //include images into your bundle
@@ -6,6 +7,33 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color,setColor]=useState("red")
+	const [colors,setColors]=useState(["yellow","green","red"]);
+	const [purple,setPurple]=useState(false);
+	const [flag,setFlag]=useState(false);
+
+	const changeColor=()=>{
+		console.log(colors);
+		let current=colors.shift();
+		setColor(current);
+		colors.push(current);
+		setColors(colors);
+	};
+
+	const addPurple=()=>{
+		for(let i=0;i<colors.length;i++){
+			if(colors[i]==="green"){
+				let first=colors.slice(0,i+1)
+				first.push("purple");
+				let second=colors.slice(i+1,colors.length);
+				 setColors([...first,...second])
+				 console.log(colors);
+			}
+		}
+	};
+
+	//setInterval(changeColor,4000)
+
 	return (
 		<div className="container mx-auto justify-content-center">
 			<div className="col-1 bg-dark mx-auto">
@@ -16,8 +44,10 @@ const Home = () => {
 				<br></br>
 			</div>
 			<div className="col-4 p-2 py-5 bg-dark mx-auto">
-				<TrafficLight/>
+				<TrafficLight setColor={setColor} color={color} purple={purple}/>
 			</div>
+			<button className="" onClick={changeColor}>Change Light</button>
+			<button className="" onClick={()=>{setPurple(!purple);setFlag(true);!flag?addPurple():null;}}>Add purple</button>
 		</div>
 	);
 };
